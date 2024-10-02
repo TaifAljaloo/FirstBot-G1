cd5c72cf77cc2d233b16335f6f11c18701815f26
Joao Manuel <joao.fernandes-lopes@etu.u-bordeaux.fr>
2024-10-02 11:13:41 +0200

    Working live gray histo

--- cam.py
+++ cam.py
@@ -12,9 +12,12 @@ cam.open("/dev/v4l/by-id/usb-046d_0825_C9049F60-video-index0")
 frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 600)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
+
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (frame_width, frame_height))
+# out = cv2.VideoWriter('output.mp4', fourcc, 30.0, (640, 480))
 fps = 0
 
 num_frames = 30
@@ -40,7 +43,7 @@ while True:
 
   width = 640
   height = 480
-  resize = cv2.resize(frame, (width, height))
+  # resize = cv2.resize(frame, (width, height))
 
   # Convert the frame to HSV color space
   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -58,15 +61,21 @@ while True:
   mask = mask1 | mask2
 
   # Bitwise-AND mask and original image
-  resize = cv2.bitwise_and(resize, resize, mask=mask)
-  gray = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY) 
+  frame = cv2.bitwise_and(frame, frame, mask=mask)
+  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
 
   # Write the frame to the output file
-  out.write(gray)
+  # out.write(gray)
 
 
   # Display the captured frame and histogram
   cv2.imshow('Camera', gray)
+  # Ignore black color in the histogram
+  gray_nonzero = gray[gray > 0]
+  plt.hist(gray_nonzero.ravel(), 256, [1, 256], color='gray')
+  plt.draw()
+  plt.pause(0.1)
+  plt.clf()
 
 
 
@@ -77,5 +86,5 @@ while True:
 
 # Release the capture and writer objects
 cam.release()
-out.release()
+# out.release()
 cv2.destroyAllWindows()
