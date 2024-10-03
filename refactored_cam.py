06848fb1ca6e1c322464936b5936d3bfadf8d136
Gaetan Cantelobre <gaetancantelobre@gmail.com>
2024-10-03 14:34:16 +0200

    refactored code v1

--- refactored_cam.py
+++ refactored_cam.py
@@ -17,55 +17,31 @@ class Filter:
         return self.higher_limit
     def getName(self):
         return self.name
-        
-        
-black_filter = Filter("black",(0, 0, 0),(360, 100, 10))
-red_filter = Filter("red",(0, 150, 150),(140, 255, 255))
+      
+      
+class LineFollower:
+      def __init__(self,minimum_speed,max_speed,robot):
+            self.max_speed = max_speed
+            self.min_speed = minimum_speed
+            self.robot = robot
+      def setSpeed(self,left,right):
+            self.robot.move(left,right)
 
+def setup_cam(width,height):
+  cam = cv2.VideoCapture(0)
+  frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+  frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+  
+  cam.set(cv2.CAP_PROP_FRAME_WIDTH,width)
+  cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+  return cam 
+      
 
-
-testing = True
-# Open the default camera
-cam = cv2.VideoCapture(0)
-
-if(not testing):
-  motor = motors.motor()
-# Get the default frame width and height
-frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
-
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-
-fps = 0
-num_frames = 30
-
-while True:
-  if fps == 0:
-    start = time.time()
-
-  ret, frame = cam.read()
-
-  fps += 1
-
-  if fps == num_frames:
-    end = time.time()
-    seconds = end - start
-    print("FPS: ", num_frames / seconds)
-    fps = 0
-
-  width = 640
-  height = 480
-  # resize = cv2.resize(frame, (width, height))
-
-  # Convert the frame to HSV color space
+def get_histogramme_from_frame(hist,filter):
   hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
   # Define the range of red color in HSV
-  mask1 = cv2.inRange(hsv, red_filter.lower_limit, red_filter.higher_limit)
+  mask1 = cv2.inRange(hsv, filter.lower_limit, filter.higher_limit)
 
   # No need for a second mask for blue as it doesn't wrap around the HSV spectrum
   mask2 = mask1
@@ -90,7 +66,11 @@ while True:
 
   # Normalize the histogram
   cv2.normalize(hist, hist, 0, 255, cv2.NORM_MINMAX)
+  return hist,gray
 
+def get_speeds():
+  # Convert the frame to  HSV color space
+  hist,gray = get_histogramme_from_frame()
   # Create an image to display the histogram
   hist_img = np.zeros((300, 256), dtype=np.uint8)
 
@@ -108,13 +88,12 @@ while True:
   part4 = gray[:, 3 * gray.shape[1] // 6: 4 * gray.shape[1] // 6]
   part5 = gray[:, 4 * gray.shape[1] // 6: 5 * gray.shape[1] // 6]
   part6 = gray[:, 5 * gray.shape[1] // 6:]
-  sum1 = np.sum(part1)/10000 * 15
-  sum2 = np.sum(part2)/10000 * 10
-  sum3 = np.sum(part3)/10000 * 5
-  sum4 = np.sum(part4)/10000 * 5
-  sum5 = np.sum(part5)/10000 * 10
-  sum6 = np.sum(part6)/10000 * 15
-  
+  sum1 = np.sum(part1)/100000 * 6
+  sum2 = np.sum(part2)/100000 * 4
+  sum3 = np.sum(part3)/100000 * 5
+  sum4 = np.sum(part4)/100000 * 5
+  sum5 = np.sum(part5)/100000 * 4
+  sum6 = np.sum(part6)/100000 * 6
   
   left = sum1 + sum2 + sum3
   left = left/ 100
@@ -123,24 +102,28 @@ while True:
   right = right/100
   right = round(right,2)
   
-  if(not testing):
-    motor.move(right,left)
-  print()
-  total_sum = sum1 + sum2 + sum3 + sum4 + sum5 + sum6
+  return left,right
 
 
-  print("left :"+ str(left))
-  print("right :"+ str(right))
+
+def main():
+  cam = setup_cam(320,240)      
+  black_filter = Filter("black",(0, 0, 0),(360, 100, 10))
+  red_filter = Filter("red",(0, 90, 90),(190, 255, 255))
+  robo = LineFollower(3,12,motors.motor())
   
+  while(True):
+        print(get_speeds())
+        robo.setSpeed(get_speeds())
+        
+  cam.release()
+  cv2.destroyAllWindows()
+        
+  
+      
+      
 
 
-
-  # Press 'q' to exit the loop
-  if cv2.waitKey(1) == ord('q'):
-    break
-
-
-# Release the capture and writer objects
-cam.release()
-# out.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main()
+    
