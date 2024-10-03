9300fececb06b476d2ac294712ff64ab326aaddf
Taif Aabo-Aljaloo <taifamer456@gmail.com>
2024-10-03 15:25:44 +0200

    Refactor calculation of color sums in cam_movement_black.py

--- cam_movement_black.py
+++ cam_movement_black.py
@@ -98,12 +98,12 @@ while True:
   part4 = gray[:, 3 * gray.shape[1] // 6: 4 * gray.shape[1] // 6]
   part5 = gray[:, 4 * gray.shape[1] // 6: 5 * gray.shape[1] // 6]
   part6 = gray[:, 5 * gray.shape[1] // 6:]
-  sum1 = np.sum(part1)/100000 * 6
-  sum2 = np.sum(part2)/100000 * 4
-  sum3 = np.sum(part3)/100000 * 5
-  sum4 = np.sum(part4)/100000 * 5
-  sum5 = np.sum(part5)/100000 * 4
-  sum6 = np.sum(part6)/100000 * 6
+  sum1 = np.sum(part1)/100000 * 3
+  sum2 = np.sum(part2)/100000 * 3
+  sum3 = np.sum(part3)/100000 * 3
+  sum4 = np.sum(part4)/100000 * 3
+  sum5 = np.sum(part5)/100000 * 3
+  sum6 = np.sum(part6)/100000 * 3
 
   left = sum1 + sum2 + sum3
   left = left/ 10
