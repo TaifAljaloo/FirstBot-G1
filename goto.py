fb0c382280f7ea7492b4c4bd104231a6103326ac
Z3ZEL <66113155+Z3ZEL@users.noreply.github.com>
2024-10-04 22:13:45 +1300

    [GOTO #11]

--- goto.py
+++ goto.py
@@ -3,7 +3,7 @@ from odometry import go_to_xya
 
 
 while True:
-    input = input("Enter x y theta: ")
+    str = input("Enter x y theta: ")
     x, y, theta = input.split()
     go_to_xya(float(x), float(y), float(theta))
     print("Done")
