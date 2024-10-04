509063588e96370d5a169d7de5163b0e5bcd01bb
Taif Aabo-Aljaloo <taifamer456@gmail.com>
2024-10-04 13:19:52 +0200

    adjusted facotrs on edge of screen gc

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def display_histogram(gray):
 
 def calculate_sums(gray):
     parts = [gray[:, i*gray.shape[1]//6:(i+1)*gray.shape[1]//6] for i in range(6)]
-    sums = [np.sum(part)/100000 * 5 for part in parts]
+    sums = [np.sum(part)/100000 * (3 if i in [0, 5] else 5) for i, part in enumerate(parts)]
     return sums
 
 def control_motor(sums, motor):
